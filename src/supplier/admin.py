8230c9bd1475036e5f264e724f2eead123cbdf54
Vlad Sharapa <sharapa.primary@gmail.com>
2026-04-16 23:34:09 +0300

    feat: [TASK-0003] add models to admin panel

--- src/supplier/admin.py
+++ src/supplier/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from core.models import CarModel
+from supplier.models import Supplier
+
 # Register your models here.
+admin.site.register(Supplier)
+admin.site.register(CarModel)

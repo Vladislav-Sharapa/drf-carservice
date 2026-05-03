98b00967e21c7a7bdda71f38458d2274f54786b1
Vlad Sharapa <sharapa.primary@gmail.com>
2026-05-03 23:22:18 +0300

    refactor: [TASK-0004] change routers for auth module

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 from rest_framework.routers import SimpleRouter
 from authentication.views import UserCreateViewSet
@@ -13,6 +14,7 @@ router.register("create", UserCreateViewSet, basename="user-create")
 
 urlpatterns = [
     path("account/", include(router.urls)),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
 ]

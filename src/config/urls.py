952f2c1336077ed49cce0a91184635751b0dd538
Vlad Sharapa <sharapa.primary@gmail.com>
2026-05-03 23:17:29 +0300

    refactor: [TASK-0004] add urls for app

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from supplier.views import SupplierApiView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/", include("authentication.urls")),
+    path("api/", include("order.urls")),
+    path("api/", include("supplier.urls")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/dealer/", include("dealer.urls")),
-    path("api/order/", include("order.urls")),
-    path("api/supplier/", SupplierApiView.as_view()),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
 ]
